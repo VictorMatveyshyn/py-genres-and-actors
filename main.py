1eac97293af6c69c2f20172df520a983c49ded90
Victor Matveyshin <vic_m@ukr.net>
2026-05-28 22:41:55 +0300

    Scene 2

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ from db.models import Actor
 
 
 def main() -> QuerySet:
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
+    items = ["Western", "Action", "Dramma"]
 
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    for item in items:
+        Genre.objects.create(name=item)
+
+    items = [("George", "Klooney"), ("Kianu", "Reaves"),
+             ("Scarlett", "Keegan"), ("Will", "Smith"), ("Jaden", "Smith"),
+             ("Scarlett", "Johansson")]
+
+    for item_first, item_second in items:
+        Actor.objects.create(
+            first_name=item_first,
+            last_name=item_second
+        )
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
@@ -27,7 +31,3 @@ def main() -> QuerySet:
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
-
-if __name__ == "__main__":
-    main()
